c6f1a46ef34de57cedff8bc1c8db46f1ac31c154
BlackKing72 <matheus.csilv14@gmail.com>
2025-03-14 09:39:54 -0300

    Removendo o astro-lenis e adicionando o astro manualmente

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import lenis from "astro-lenis";
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [
-        lenis(),
     ]
 });
